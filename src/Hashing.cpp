80b4e32424ed91e817887499ebed71cc8500196b
dim-chr <dimitrischristofis@gmail.com>
2021-11-01 17:00:29 +0200

    Added comments in Hashing files. Minor changes in hash table insert function.

--- src/Hashing.cpp
+++ src/Hashing.cpp
@@ -27,39 +27,41 @@ HashTable *hashTables;
 
 using namespace std;
 
+// Function that is used to initialize all the necessary variables and data structures in order to use the hash functions and the hash tables
 void init_hashing(int k, int L, int d, unsigned int TableSize)
 {
     // Init
-    srand( time(0) );
+    srand(time(0));
     
     window = rand() % 5 + 2;
     
-    hashTables = new HashTable( L, TableSize );
+    hashTables = new HashTable(L, TableSize);
     
     
-    // Ftiaxnoume ta ri
+    // Initialize the 'r' vector that will be used by every amplified hash function 'g(p)'
     {
         default_random_engine generator;
-        uniform_int_distribution<int> distribution( 1, 10 );
+        uniform_int_distribution<int> distribution(1, 10);
 
         for (int i = 0; i < k; i++) {
 
-            r.push_back( distribution(generator) );
+            r.push_back(distribution(generator));
         }
     }
 
-    // Ftiaxnoume ta t
+    // Initialize all the random 't' numbers that will be used by the hash functions 'h(p)'
     {
         default_random_engine generator;
-        uniform_real_distribution<double> distribution( 0, window );
+        uniform_real_distribution<double> distribution(0, window);
 
         for (int i = 0; i < k; i++) {
 
-            t.push_back( distribution(generator) );
+            t.push_back(distribution(generator));
         }
     }
     
-    // Ftiaxnoume ta v
+    // Initialize the all the 'v' vectors that will be used by the hash functions 'h(p)'
+	// We need k vectors 'v', one for each hash function
     {
         default_random_engine generator;
         normal_distribution<double> distribution( 0.0, 1.0 );
@@ -70,51 +72,55 @@ void init_hashing(int k, int L, int d, unsigned int TableSize)
 
             for (int j = 0; j < d; j++) {
                 
-                v[i].push_back( distribution(generator) );
+                v[i].push_back(distribution(generator));
             }
         }
     }
     
-    // Ftiaxnoume ta g
-    g.resize( L );
+    // Initialize the 'g' vectors
+	// There will be L 'g' vectors in total and each one stores the order in which the h(p) functions will be multiplied with the 'ri' numbers
+	// E.g k=4, L=5, g[0]=[0,1,3,2] then g1(p)=((r1*h1(p)+r2*h2(p)+r3*h4(p)+r4*h3(p)) mod M) mod TableSize
+    g.resize(L);
     for (int i = 0; i < L; i++) {
         
         for (int j = 0; j < k; j++) {
             
-            int h = rand() % k;
+            int h = rand() % k;  // Get a random number from 0 to k-1
 
-            // Oso yparxei h hash tote dialegei epomeni
-            while( find( g[i].begin(), g[i].end(), h ) != g[i].end() )
+            // If this number is in the g[i] vector then keep generating new ones until one that isn't already in g[i] is found
+            while(find(g[i].begin(), g[i].end(), h) != g[i].end())
             {
                 h = rand() % k;
             }
 
-            g[i].push_back(h);
+            g[i].push_back(h);  // Insert that number in g[i]
         }
     }
 
 }
 
-unsigned int h_func( const vector<unsigned long> &p, int i )
+// This is the h(p) hash function
+unsigned int h_func(const vector<unsigned long> &p, int i)
 {
     
     double dot_product=0.0;
     
-    // Diasxizoume to dianisma p gia na ypologisoume to ginomeno p*v
-    // d einai kathe oros
-    for( int j=0; j<p.size(); j++ )
+    // Calculate the dot product p*v
+    for(int j=0; j<p.size(); j++)
     {
         dot_product += p[j] * v[i][j];
     }
     
-    return (unsigned int) floor( (dot_product + t[i]) / window );
+    return (unsigned int) floor((dot_product + t[i]) / window);
 }
 
-unsigned int g_func( const vector<unsigned long> &p, unsigned int TableSize, int i)
+// This is the amplified hash function g(p)
+unsigned int g_func(const vector<unsigned long> &p, unsigned int TableSize, int i)
 {
     unsigned long sum=0;
     
-    for( int j=0; j<g[0].size(); j++ )
+	// Calculate the sum r1*h1(p) + r2*h2(p) +...
+    for(int j=0; j<g[0].size(); j++)
     {
         sum += r[j] * h_func(p, g[i][j]);
     }
@@ -125,19 +131,19 @@ unsigned int g_func( const vector<unsigned long> &p, unsigned int TableSize, int
 
 /*======================================================*/
 
-
-pair< string, vector<unsigned long>> * VectorData::insert(string id, const vector<unsigned long> &v )
+// Function that is used to insert each vector in the list
+pair<string, vector<unsigned long>> * VectorData::insert(string id, const vector<unsigned long> &v)
 {
-    vectors.push_back( make_pair( id, v) );
+    vectors.push_back(make_pair(id, v));  // Insert the 'item_id' of vector 'p' and its coordinates
     
-    // Pairnoume to teleutaio stoixeio (diladi auto pou molis mmpike)
-    // gia na mathoume tin dieuthinsi mnimis tou
+    // Get the item that was just inserted in the list
     pair< string, vector<unsigned long>> &p = vectors.back();
     
-    // epistrefoume tin DM tou
+    // Return the item's address
     return &p;
 }
 
+// Function that returns the size of the list
 unsigned int VectorData::size()
 {
     return vectors.size();
@@ -146,25 +152,28 @@ unsigned int VectorData::size()
 
 /*=======================================================*/
 
+// Constructor of HashTable class
 HashTable::HashTable(int L, unsigned int TableSize)
 {
     this->L = L;
     this->TableSize = TableSize;
 
-    // Ftiaxnw posous pinakes thwloume
-    hashTables.resize( L );
+    // L hash tables are needed in total
+    hashTables.resize(L);
 
-    // Gia kathe pinaka katakermatismou ftiaxnw tous koubades tou
+    // Each hash table hash 'TableSize' buckets
     for (int i = 0; i < L; i++) {
-        hashTables[i].resize( TableSize);
+        hashTables[i].resize(TableSize);
     }
 }
 
-void HashTable::insert( int l, string id, vector<unsigned long> &p )
+void HashTable::insert(int l, string id, vector<unsigned long> &p)
 {
     // An to l einai stous pinakes katakermatismou
-    if( l < this->L )
+    if(l < this->L)
     {
-        
+        hashValue = g_func(p, this->TableSize, l);
+		
+		hashTables[l][hashValue].push_back();
     }
 }
--- src/Hashing.h
+++ src/Hashing.h
@@ -9,28 +9,33 @@
 
 using namespace std;
 
+// Class that is used to store all the vectors 'p' from the input file
 class VectorData
 {
     private:
-    list< pair< string, vector<unsigned long> > >   vectors;
+    list< pair<string, vector<unsigned long>> >   vectors;  // Each node of this list contains the 'item_id' of each vector 'p' and its coordinates
     
     public:
-        pair< string, vector<unsigned long>> * insert(string id, const vector<unsigned long> &v );
-        unsigned int size();
+        pair<string, vector<unsigned long>> * insert(string id, const vector<unsigned long> &v );  // Function that is used to insert each vector in the list
+        unsigned int size();  // Function that returns the size of the list
 };
 
+// Class that contains all the hash tables that will be used, as well as the number ('L') of hash tables and the size ('TableSize') of each one
 class HashTable
 {
     private:
-        int L;
-        unsigned int TableSize;
+        int L;  // Number of hash tables
+        unsigned int TableSize;  // Number of buckets for each hash table
         
-        vector<vector<vector<pair< unsigned int, pair< string, vector<unsigned long>> * >>>> hashTables;
+		// These are all the hash tables that will be used
+		// Every bucket of each hash table contains an ID (ID(p) = r1*h1(p)+r2*h2(p)+... mod M) for the item that will be inserted
+		// Each bucket also contains a pointer to a pair (item_id, vector 'p') from the list of vectors in class 'VectorData'
+        vector<vector<vector< pair< unsigned int, pair<string, vector<unsigned long>> * >>>> hashTables;
     
     public:
     
-        HashTable(int L, unsigned int TableSize);
-        void insert( int l, string id, vector<unsigned long> &p );
+        HashTable(int L, unsigned int TableSize);  // Constructor
+        void insert(int l, string id, vector<unsigned long> &p);  // Function that inserts an item in the hash table
 };
 
 
@@ -39,11 +44,11 @@ extern HashTable *hashTables;
 
 void init_hashing(int k);
 
-unsigned int h_func( const vector<unsigned long> &p, int i );
+unsigned int h_func(const vector<unsigned long> &p, int i);
 
-unsigned int g_func( const vector<unsigned long> &p, unsigned int TableSize, int i);
+unsigned int g_func(const vector<unsigned long> &p, unsigned int TableSize, int i);
 
 
 
-#endif /* HASHING_H */
+#endif
 
